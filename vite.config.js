7f50c9e83c19a26ed784ae7eac671543fcbc56ba
1602-cai <caiy47768@gmail.com>
2026-05-14 14:56:05 +0800

    更新配置

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/ti-ku-wang-zhan/'  // 改为你的仓库名
-    : './',
+  base: '/ti-ku-wang-zhan/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
